71c36550fd8ab2abf5faac6e823bfa7cbd45fccd
Adelino Segundo <adelinosegundo@gmail.com>
2015-11-23 23:42:45 -0300

    stuff

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ActiveRecord::Base
 	has_many :sensors
 
 	def get_sensor_by_highest_value
-		sensors.sort_by(&:latest_state_value).first
+		sensors.map(&:latest_state_value).max
 	end
 end
